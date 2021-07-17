21296212e5c680b21039c49c5fadd23ad47adccd
Ian V <ianenrico1@gmail.com>
2021-07-17 13:48:15 -0400

    Navbar functionality completed.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
       <h2>Made For You</h2>
       <div className=" text-2xl mt-8 flex flex-col md:flex-row ">
         <button
-          className="font-bold  w-48 mx-8 my-2 border-2 px-4 py-2 hover:text-gray-900
+          className="font-bold text-shadow-xl w-48 mx-8 my-2 border-2 px-4 py-2 hover:text-gray-900
         hover:border-gray-900"
         >
           <a href="tel:12892346622">Order Now</a>
         </button>
         <button
-          className="font-bold w-48 mx-8 my-2 border-2 px-4 py-2 hover:text-gray-900
+          className="font-bold text-shadow-xl w-48 mx-8 my-2 border-2 px-4 py-2 hover:text-gray-900
         hover:border-gray-900"
         >
           Find Us
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,38 @@
+import React, { useState, useCallback, useEffect } from "react";
+
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
 
+const useMediaQuery = (width) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addEventListener("change", (e) => updateTarget(e));
+
+    if (media.matches) {
+      setTargetReached(true);
+    }
+    return () => {
+      media.removeEventListener("change", (e) => updateTarget(e));
+    };
+  }, []);
+
+  return targetReached;
+};
+
 const Navbar = () => {
-  return (
-    <div>
-      <MobileNav />
-      {/* <DesktopNav/> */}
-    </div>
-  );
+  const isBreakpoint = useMediaQuery(767);
+
+  return <div>{isBreakpoint ? <MobileNav /> : <DesktopNav />}</div>;
 };
 
 export default Navbar;
